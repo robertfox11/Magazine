9bbcf3428a4b0d7d31fd40de5bdff1d45a7ece23
robert Salazar <11rsahome@gmail.com>
2020-03-11 13:00:35 +0100

    add index principal

--- config/DataBase.php
+++ config/DataBase.php
@@ -2,7 +2,7 @@
 class Database{
     
         public static function connect(){
-            $db = new mysqli('localhost:3308', 'root', '', 'databaseblog');
+            $db = new mysqli('localhost:3308', 'root', '', 'databasemagazine');
             $db->query("SET NAMES 'utf8'");
             if ($db->connect_errno) {
                 printf("connectar failed", $db->connect_error);
--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-    define("URL", "http://localhost/ProyectoBlogMedida/");
+    define("URL", "http://localhost/appMagazine/");
     //Controlador por defecto
     define('controller_default','CommentsController');
     define('action_default', 'index');
