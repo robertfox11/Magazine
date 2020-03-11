07437b4b30da0cce8a73478148dfc088085e1c3f
robert Salazar <11rsahome@gmail.com>
2020-03-11 13:52:34 +0100

    layouts styles add boostrap

--- views/layout/header.php
+++ views/layout/header.php
@@ -2,22 +2,23 @@
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?= URL ?>assets/css/default.css">
-    <title></title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
+	<link rel="stylesheet" href="<?= URL ?>assets/bootstrap-4.3.1/dist/css/bootstrap.min.css">
+	<title></title>
 </head>
 
-<body>
-    <header id="header">
-        <h1 id="blog">Magazine</h1>
+<body class="bg-primary">
+	<header class="" id="header">
+		<h1 class="text-light" id="">Magazine</h1>
 		<nav id="menu">
 			<ul>
 				<li>
-				<a href="<?= URL ?>">Inicio</a>
+					<a href="<?= URL ?>">Inicio</a>
 				</li>
 
 			</ul>
 		</nav>
-    </header>
+	</header>
